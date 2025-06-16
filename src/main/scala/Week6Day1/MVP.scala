4661a0e62c1b7921734267f9ee6ea51ed39fd15a
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-06-16 14:02:54 +0100

    Added Week6Day1 files

---
+++ src/main/scala/Week6Day1/MVP.scala
@@ -0,0 +1,59 @@
+package Week6Day1
+
+
+import org.openqa.selenium.{By, OutputType, TakesScreenshot, WebDriver, WebElement}
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.io.FileHandler
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object MVP extends App {
+
+  //1. Navigate to test page
+
+  val options = new ChromeOptions() //set up chromeoptions
+  options.addArguments("--headless")// parse in the arguments
+
+  val driver: WebDriver = new ChromeDriver(options) // create object for browser
+driver.get("https://the-internet.herokuapp.com/login") //navigate to test site
+  println("Navigate to test page - pass")
+
+  driver.manage().window().maximize() // to maximise window
+  println("Maximise - pass")
+
+
+  val username: WebElement = driver.findElement(By.name("username"))
+  username.sendKeys("tomsmith")
+  println("Username entered - pass")
+
+
+  val password: WebElement = driver.findElement(By.name("password"))
+  password.sendKeys("SuperSecretPassword!")
+  println("password entered - pass")
+
+
+  val loginButton = driver.findElement(By.xpath("//*[@id='login']/button"))
+  loginButton.click()
+  println("Login button clicked successfully")
+
+  // Save static screenshot
+  val srcFull: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+  FileHandler.copy(srcFull, new File("/Users/shantel.shaw/Desktop/HandlingScreenShots/fullpage.png"))
+
+  // Save timestamped screenshot
+  val timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())
+  val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+  FileHandler.copy(srcTimestamp, new File(
+    s"/Users/shantel.shaw/Desktop/HandlingScreenShots/TestResult_$timeStamp.png"))
+  println("Screenshot successful")
+
+
+  // do try catch statement
+
+  driver.quit()
+
+
+}
+
