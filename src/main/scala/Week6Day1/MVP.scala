79d7f8d6abeab74ca12e6d5f15dac26f6e051993
Shantel Shaw <shantel_shaw@hotmail.co.uk>
2025-06-17 09:58:06 +0100

    MVP completed

--- src/main/scala/Week6Day1/MVP.scala
+++ src/main/scala/Week6Day1/MVP.scala
@@ -11,10 +11,10 @@ import java.util.Date
 
 object MVP extends App {
 
-  //1. Navigate to test page
+  //1. Navigate to test page - headless
 
   val options = new ChromeOptions() //set up chromeoptions
-  options.addArguments("--headless")// parse in the arguments
+  options.addArguments("--headless=new")// parse in the arguments
 
   val driver: WebDriver = new ChromeDriver(options) // create object for browser
 driver.get("https://the-internet.herokuapp.com/login") //navigate to test site
@@ -23,34 +23,35 @@ driver.get("https://the-internet.herokuapp.com/login") //navigate to test site
   driver.manage().window().maximize() // to maximise window
   println("Maximise - pass")
 
-
+//2. input username
   val username: WebElement = driver.findElement(By.name("username"))
   username.sendKeys("tomsmith")
   println("Username entered - pass")
 
-
+//3 input password
   val password: WebElement = driver.findElement(By.name("password"))
   password.sendKeys("SuperSecretPassword!")
   println("password entered - pass")
 
-
+//4 login via xpath
   val loginButton = driver.findElement(By.xpath("//*[@id='login']/button"))
   loginButton.click()
   println("Login button clicked successfully")
 
-  // Save static screenshot
+  // 5 Save static screenshot
   val srcFull: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
   FileHandler.copy(srcFull, new File("/Users/shantel.shaw/Desktop/HandlingScreenShots/fullpage.png"))
 
-  // Save timestamped screenshot
+  // 6 Save timestamped screenshot
   val timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())
   val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
-  FileHandler.copy(srcTimestamp, new File(
-    s"/Users/shantel.shaw/Desktop/HandlingScreenShots/TestResult_$timeStamp.png"))
+  FileHandler.copy(srcTimestamp, new File(s"/Users/shantel.shaw/Desktop/HandlingScreenShots/TestResult_$timeStamp.png"))
   println("Screenshot successful")
 
 
   // do try catch statement
+
+  //7 quit test
 
   driver.quit()
 
